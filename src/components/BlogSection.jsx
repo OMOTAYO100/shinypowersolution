a2f1d515af726800f3fc0efad98b805df20864fc
OMOTAYO100 <oshiboteomotayo100@gmail.com>
2025-10-31 13:23:50 +0100

    fix: resolved missing SolarNewsData file

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { solarNewsData } from "../data/solarNewsData";
+import { solarNewsData } from "../data/SolarNewsData";
 import { Link } from "react-router-dom";
 
 function BlogSection() {
