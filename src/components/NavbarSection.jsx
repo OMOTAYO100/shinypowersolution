6b890a6fe37af6856b8330416e29c48ebde8acf7
OMOTAYO100 <oshiboteomotayo100@gmail.com>
2025-11-03 08:52:22 +0100

    first commit

--- src/components/NavbarSection.jsx
+++ src/components/NavbarSection.jsx
@@ -20,13 +20,13 @@ const NavbarSection = () => {
             <p className="text-lg md:text-md font-semibold">SHIN<span className="text-green-500">Y</span></p>
           </div>
           <ul
-            className={` flex flex-col md:gap-2  bg-green-500/60 fixed rounded top-0 right-0 backdrop-blur-lg p-24 ${
+            className={` flex flex-col md:gap-2  bg-green-500/60 fixed rounded top-0 right-0 backdrop-blur-lg p-24 gap-4 ${
               isOpen ? "scale-y-100" : "scale-y-0 "
             } origin-top z-30 transition-transform duration-300 md:flex-row md:scale-y-100 md:backdrop-blur-0 md:static md:p-2 md:bg-transparent`}
           >
             <li className="m-2 hover:scale-110 transition-transform duration-300">
               <Link to="/"
-                className="py-2 text-sm md:text-md lg:text-lg hover:text-md transition-all duration-300 hover:text-green-500 0"
+                className="py-2 text-sm md:text-md lg:text-lg hover:text-md transition-all duration-300 hover:text-green-500"
               >
                 Home
               </Link>
