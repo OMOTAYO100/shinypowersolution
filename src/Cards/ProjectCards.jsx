791bedef53dc5a8bfa1d733442a50ad3319ac5bc
OMOTAYO100 <oshiboteomotayo100@gmail.com>
2025-11-04 16:06:29 +0100

    first commit

--- src/Cards/ProjectCards.jsx
+++ src/Cards/ProjectCards.jsx
@@ -7,7 +7,7 @@ function ProjectCards({ title, image, text }) {
               <figure className="relative inline-block overflow-hidden rounded-lg shadow-md hover:scale-105 transition-all duration-300">
                 <img className="w-[100%] block" src={image} alt="" />
                 <figcaption
-                className="flex items-center gap-2 absolute bottom-25 md:bottom-30 left-10 md:left-[-100%] text-green-500 font-bold text-xl md:text-2xl md:opacity-0 
+                className="flex items-center gap-2 absolute top-5 md:top-15 left-10 md:left-[-100%] font-bold text-xl md:text-2xl md:opacity-0 
                    transition-all duration-700 ease-in-out md:group-hover:left-10 md:group-hover:opacity-100"
               >
                 {title}
