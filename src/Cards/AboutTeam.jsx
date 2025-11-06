456e58ee5e05b46194644cfd599268adb3fabcdc
OMOTAYO100 <oshiboteomotayo100@gmail.com>
2025-11-06 14:54:33 +0100

    first commit

--- src/Cards/AboutTeam.jsx
+++ src/Cards/AboutTeam.jsx
@@ -9,7 +9,7 @@ function AboutTeam({ image, name, text, dataAos, dataAosDelay }) {
   return (
     <>
       <div className="flex flex-col items-center bg-white shadow-md rounded-2xl p-4 hover:shadow-lg transition" data-aos={dataAos} data-aos-delay={dataAosDelay}>
-        <figure className="w-40 h-40 rounded-xl overflow-hidden mb-4">
+        <figure className="w-full max-h-fit rounded-xl overflow-hidden mb-4">
           <img src={image} alt={name} className="w-full h-full object-cover" />
         </figure>
         <h3 className="text-lg font-semibold">{name}</h3>
--- src/components/AboutHistory.jsx
+++ src/components/AboutHistory.jsx
@@ -101,7 +101,7 @@ function AboutHistory({ title, subtitle, description }) {
       <section className="mt-16 p-6 md:p-12 bg-slate-100 rounded-2xl">
         <h2 className="text-3xl font-bold text-center mb-10">Meet Our Team</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {aboutData.map((team) => (
             <AboutTeam
               key={team.id}
